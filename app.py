50a26b3f6fd067d618607e90fd6a7262925ba4da
Andrew Cowan <100159853+agcowan@users.noreply.github.com>
2022-05-21 21:07:17 -0400

    Completion of analysis and exploration portion. Initial work on climate app completed

---
+++ app.py
@@ -0,0 +1,93 @@
+from lib2to3.pytree import Base
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+#################################################
+# Database setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine,reflect=True)
+
+# References to tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def Home():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # create session
+    session = Session(engine)
+    results = session.query(Measurement.date,Measurement.prcp).all()
+    # close session
+    session.close()
+    all_precip = []
+
+    for date,prcp in results:
+        date_dict = {}
+        date_dict["date"] = date
+        date_dict["prcp"] = prcp
+        all_precip.append(date_dict)
+
+    return jsonify(all_precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+     # create session
+    session = Session(engine)
+    locations = session.query(Station.station).all()
+    # close session
+    session.close()
+    stations = list(np.ravel(locations))
+    return jsonify(stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # create session
+    session = Session(engine)
+    last_year = session.query(Measurement.date,Measurement.tobs).\
+        filter(Measurement.date>='2016-08-23').\
+        filter(Measurement.station == 'USC00519281').\
+        order_by(Measurement.date).all()
+    # close session
+    session.close()
+    temps = list(np.ravel(last_year))
+    return jsonify(temps)
+
+@app.route("/api/v1.0/<start>")
+def aggregate1(start):
+    # create session
+    session = Session(engine)
+    min_temp = session.query(func.min(Measurement.tobs)).all()
+    max_temp = session.query(func.max(Measurement.tobs)).all()
+    avg_temp = session.query(func.avg(Measurement.tobs)).all()
+    # close session
+    session.close()
+# Boilerplate
+if __name__ == '__main__':
+    app.run(debug=True)
