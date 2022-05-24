c98579804fe5550ec41d4ed5d3c515b231e7476f
Andrew Cowan <100159853+agcowan@users.noreply.github.com>
2022-05-23 20:55:01 -0400

    Completed notebook and the first bonus activity and the app.py file for completion of assignment.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # sqlalchemy-challenge
+The hawaii-sqlite database is used to provide data on years-long temperature and precipitation measurements at various observation stations. The analysis is then graphed with a line graph to show precipitation over time.
+
+The station analysis portion counts the number of stations, shows which station is the most active, and then runs a histogram of the frequency of temperature observations at that particular station.
+
+The bonus section of the notebook file contains a converted DataFrame that takes the date and converts it to a datetime object. From there, I created two DataFrames to pull out all dates for the months of June and December to run T-Tests on that data.
+
+In app.py, the created route has pages for precipitation, stations, observed temperatures, and two functionalities that take user input. The first tool is where a user can input a start date in YYYY-MM-DD format and the return is the min, max, and average temperature for all stations starting from that point to the end of the available data. The second tool takes in a start and end date in YYYY-MM-DD format to return min, max, and average temperature for all stations beginning with the input start date and ending with the input end date.
--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from lib2to3.pytree import Base
 import numpy as np
-import pandas as pd
-import datetime as dt
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -15,7 +12,7 @@ app = Flask(__name__)
 #################################################
 # Database setup
 #################################################
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine,reflect=True)
 
@@ -26,19 +23,20 @@ Station = Base.classes.station
 #################################################
 # Flask Routes
 #################################################
-
+# Home
 @app.route("/")
 def Home():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f'<a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a><br/>'
+        f'<a href="/api/v1.0/stations">/api/v1.0/stations</a><br/>'
+        f'<a href="/api/v1.0/tobs">/api/v1.0/tobs</a><br/>'
+        f'<a href="/api/v1.0/&lt;start&gt;">/api/v1.0/&lt;start&gt;</a> Use date in YYYY-MM-DD format<br/>' # remind user to put date as YYYY-MM-DD
+        f'<a href="/api/v1.0/&lt;start&gt;/&lt;end&gt;">/api/v1.0/&lt;start&gt;/&lt;end&gt;</a> Use date in YYYY-MM-DD format'
     )
 
+# Precipitation
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     # create session
@@ -56,6 +54,7 @@ def precipitation():
 
     return jsonify(all_precip)
 
+# Stations
 @app.route("/api/v1.0/stations")
 def stations():
      # create session
@@ -66,6 +65,7 @@ def stations():
     stations = list(np.ravel(locations))
     return jsonify(stations)
 
+# Temperature observations
 @app.route("/api/v1.0/tobs")
 def tobs():
     # create session
@@ -79,15 +79,30 @@ def tobs():
     temps = list(np.ravel(last_year))
     return jsonify(temps)
 
+# Search all dates from input start date until end
 @app.route("/api/v1.0/<start>")
 def aggregate1(start):
     # create session
     session = Session(engine)
-    min_temp = session.query(func.min(Measurement.tobs)).all()
-    max_temp = session.query(func.max(Measurement.tobs)).all()
-    avg_temp = session.query(func.avg(Measurement.tobs)).all()
+    min_temp = session.query(func.min(Measurement.tobs)).filter(Measurement.date>start).all()
+    max_temp = session.query(func.max(Measurement.tobs)).filter(Measurement.date>start).all()
+    avg_temp = session.query(func.avg(Measurement.tobs)).filter(Measurement.date>start).all()
     # close session
     session.close()
+    return jsonify(f'Min: {min_temp[0][0]}, Max: {max_temp[0][0]}, Average: {round(avg_temp[0][0],2)}')
+
+# Search all dates from input start date until input end date
+@app.route("/api/v1.0/<start>/<end>")
+def aggregate2(start,end):
+    # create session
+    session = Session(engine)
+    min_temp = session.query(func.min(Measurement.tobs)).filter(Measurement.date>start).filter(Measurement.date<end).all()
+    max_temp = session.query(func.max(Measurement.tobs)).filter(Measurement.date>start).filter(Measurement.date<end).all()
+    avg_temp = session.query(func.avg(Measurement.tobs)).filter(Measurement.date>start).filter(Measurement.date<end).all()
+    # close session
+    session.close()
+    return jsonify(f'Min: {min_temp[0][0]}, Max: {max_temp[0][0]}, Average: {round(avg_temp[0][0],2)}')
+
 # Boilerplate
 if __name__ == '__main__':
     app.run(debug=True)
